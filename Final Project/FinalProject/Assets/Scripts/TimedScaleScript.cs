6e2db4002f2729fc1c5f077c5e1c09471eacca01
friikjones <d.mullerjones@gmail.com>
2020-01-08 15:38:01 -0800

    Added 6.1

---
+++ Final Project/FinalProject/Assets/Scripts/TimedScaleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedScaleScript : MonoBehaviour
+{
+    public float scaleSpeed;
+    public int maxSize;
+
+    public Vector3 initialScale;
+    public RingInstantiateScript gameManagerScript;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localScale = initialScale;
+        gameManagerScript = GameObject.FindWithTag("Game Manager").GetComponent<RingInstantiateScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scaleSpeed = gameManagerScript.ringSpeed;
+        this.transform.localScale = this.transform.localScale + new Vector3(scaleSpeed * Time.deltaTime, scaleSpeed * Time.deltaTime, 0);
+
+        if(this.transform.localScale.x > maxSize){
+            Destroy(gameObject);
+        }
+    }
+}
